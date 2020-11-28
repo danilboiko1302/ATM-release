8b193965537808542fa41d76b2aa6be01d285030
danilboiko1302 <51995092+danilboiko1302@users.noreply.github.com>
2020-11-28 19:36:48 +0200

    update

--- moc_mainwindow.cpp
+++ moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[39];
-    char stringdata0[449];
+    QByteArrayData data[31];
+    char stringdata0[446];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,58 +33,50 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 2), // "s1"
-QT_MOC_LITERAL(2, 14, 0), // ""
-QT_MOC_LITERAL(3, 15, 2), // "s3"
-QT_MOC_LITERAL(4, 18, 2), // "s4"
-QT_MOC_LITERAL(5, 21, 2), // "s5"
-QT_MOC_LITERAL(6, 24, 2), // "b1"
-QT_MOC_LITERAL(7, 27, 2), // "b2"
-QT_MOC_LITERAL(8, 30, 2), // "b3"
-QT_MOC_LITERAL(9, 33, 2), // "b4"
-QT_MOC_LITERAL(10, 36, 2), // "b5"
-QT_MOC_LITERAL(11, 39, 2), // "b6"
-QT_MOC_LITERAL(12, 42, 2), // "b7"
-QT_MOC_LITERAL(13, 45, 2), // "b8"
-QT_MOC_LITERAL(14, 48, 2), // "b9"
-QT_MOC_LITERAL(15, 51, 3), // "b10"
-QT_MOC_LITERAL(16, 55, 21), // "on_insertCard_clicked"
-QT_MOC_LITERAL(17, 77, 18), // "on_screen1_clicked"
-QT_MOC_LITERAL(18, 96, 18), // "on_screen2_clicked"
-QT_MOC_LITERAL(19, 115, 18), // "on_screen3_clicked"
-QT_MOC_LITERAL(20, 134, 18), // "on_screen4_clicked"
-QT_MOC_LITERAL(21, 153, 18), // "on_screen5_clicked"
-QT_MOC_LITERAL(22, 172, 18), // "on_screen6_clicked"
-QT_MOC_LITERAL(23, 191, 18), // "on_screen7_clicked"
-QT_MOC_LITERAL(24, 210, 18), // "on_screen8_clicked"
-QT_MOC_LITERAL(25, 229, 16), // "on_reset_clicked"
-QT_MOC_LITERAL(26, 246, 13), // "on_b1_clicked"
-QT_MOC_LITERAL(27, 260, 13), // "on_b2_clicked"
-QT_MOC_LITERAL(28, 274, 13), // "on_b3_clicked"
-QT_MOC_LITERAL(29, 288, 13), // "on_b4_clicked"
-QT_MOC_LITERAL(30, 302, 13), // "on_b5_clicked"
-QT_MOC_LITERAL(31, 316, 13), // "on_b6_clicked"
-QT_MOC_LITERAL(32, 330, 13), // "on_b7_clicked"
-QT_MOC_LITERAL(33, 344, 13), // "on_b8_clicked"
-QT_MOC_LITERAL(34, 358, 13), // "on_b9_clicked"
-QT_MOC_LITERAL(35, 372, 13), // "on_ok_clicked"
-QT_MOC_LITERAL(36, 386, 17), // "on_cancel_clicked"
-QT_MOC_LITERAL(37, 404, 21), // "on_insertCash_clicked"
-QT_MOC_LITERAL(38, 426, 22) // "on_settingsBtn_clicked"
+QT_MOC_LITERAL(1, 11, 13), // "changeSumCash"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 2), // "s1"
+QT_MOC_LITERAL(4, 29, 2), // "s3"
+QT_MOC_LITERAL(5, 32, 2), // "s4"
+QT_MOC_LITERAL(6, 35, 2), // "s5"
+QT_MOC_LITERAL(7, 38, 21), // "on_insertCard_clicked"
+QT_MOC_LITERAL(8, 60, 18), // "on_screen1_clicked"
+QT_MOC_LITERAL(9, 79, 18), // "on_screen2_clicked"
+QT_MOC_LITERAL(10, 98, 18), // "on_screen3_clicked"
+QT_MOC_LITERAL(11, 117, 18), // "on_screen4_clicked"
+QT_MOC_LITERAL(12, 136, 18), // "on_screen5_clicked"
+QT_MOC_LITERAL(13, 155, 18), // "on_screen6_clicked"
+QT_MOC_LITERAL(14, 174, 18), // "on_screen7_clicked"
+QT_MOC_LITERAL(15, 193, 18), // "on_screen8_clicked"
+QT_MOC_LITERAL(16, 212, 16), // "on_reset_clicked"
+QT_MOC_LITERAL(17, 229, 13), // "on_b1_clicked"
+QT_MOC_LITERAL(18, 243, 13), // "on_b2_clicked"
+QT_MOC_LITERAL(19, 257, 13), // "on_b3_clicked"
+QT_MOC_LITERAL(20, 271, 13), // "on_b4_clicked"
+QT_MOC_LITERAL(21, 285, 13), // "on_b5_clicked"
+QT_MOC_LITERAL(22, 299, 13), // "on_b6_clicked"
+QT_MOC_LITERAL(23, 313, 13), // "on_b7_clicked"
+QT_MOC_LITERAL(24, 327, 13), // "on_b8_clicked"
+QT_MOC_LITERAL(25, 341, 13), // "on_b9_clicked"
+QT_MOC_LITERAL(26, 355, 13), // "on_ok_clicked"
+QT_MOC_LITERAL(27, 369, 17), // "on_cancel_clicked"
+QT_MOC_LITERAL(28, 387, 21), // "on_insertCash_clicked"
+QT_MOC_LITERAL(29, 409, 22), // "on_settingsBtn_clicked"
+QT_MOC_LITERAL(30, 432, 13) // "on_b0_clicked"
 
     },
-    "MainWindow\0s1\0\0s3\0s4\0s5\0b1\0b2\0b3\0b4\0"
-    "b5\0b6\0b7\0b8\0b9\0b10\0on_insertCard_clicked\0"
-    "on_screen1_clicked\0on_screen2_clicked\0"
-    "on_screen3_clicked\0on_screen4_clicked\0"
-    "on_screen5_clicked\0on_screen6_clicked\0"
-    "on_screen7_clicked\0on_screen8_clicked\0"
-    "on_reset_clicked\0on_b1_clicked\0"
-    "on_b2_clicked\0on_b3_clicked\0on_b4_clicked\0"
-    "on_b5_clicked\0on_b6_clicked\0on_b7_clicked\0"
-    "on_b8_clicked\0on_b9_clicked\0on_ok_clicked\0"
-    "on_cancel_clicked\0on_insertCash_clicked\0"
-    "on_settingsBtn_clicked"
+    "MainWindow\0changeSumCash\0\0s1\0s3\0s4\0"
+    "s5\0on_insertCard_clicked\0on_screen1_clicked\0"
+    "on_screen2_clicked\0on_screen3_clicked\0"
+    "on_screen4_clicked\0on_screen5_clicked\0"
+    "on_screen6_clicked\0on_screen7_clicked\0"
+    "on_screen8_clicked\0on_reset_clicked\0"
+    "on_b1_clicked\0on_b2_clicked\0on_b3_clicked\0"
+    "on_b4_clicked\0on_b5_clicked\0on_b6_clicked\0"
+    "on_b7_clicked\0on_b8_clicked\0on_b9_clicked\0"
+    "on_ok_clicked\0on_cancel_clicked\0"
+    "on_insertCash_clicked\0on_settingsBtn_clicked\0"
+    "on_b0_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -94,7 +86,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      37,   14, // methods
+      29,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -102,54 +94,38 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  199,    2, 0x08 /* Private */,
-       3,    0,  200,    2, 0x08 /* Private */,
-       4,    0,  201,    2, 0x08 /* Private */,
-       5,    0,  202,    2, 0x08 /* Private */,
-       6,    0,  203,    2, 0x08 /* Private */,
-       7,    0,  204,    2, 0x08 /* Private */,
-       8,    0,  205,    2, 0x08 /* Private */,
-       9,    0,  206,    2, 0x08 /* Private */,
-      10,    0,  207,    2, 0x08 /* Private */,
-      11,    0,  208,    2, 0x08 /* Private */,
-      12,    0,  209,    2, 0x08 /* Private */,
-      13,    0,  210,    2, 0x08 /* Private */,
-      14,    0,  211,    2, 0x08 /* Private */,
-      15,    0,  212,    2, 0x08 /* Private */,
-      16,    0,  213,    2, 0x08 /* Private */,
-      17,    0,  214,    2, 0x08 /* Private */,
-      18,    0,  215,    2, 0x08 /* Private */,
-      19,    0,  216,    2, 0x08 /* Private */,
-      20,    0,  217,    2, 0x08 /* Private */,
-      21,    0,  218,    2, 0x08 /* Private */,
-      22,    0,  219,    2, 0x08 /* Private */,
-      23,    0,  220,    2, 0x08 /* Private */,
-      24,    0,  221,    2, 0x08 /* Private */,
-      25,    0,  222,    2, 0x08 /* Private */,
-      26,    0,  223,    2, 0x08 /* Private */,
-      27,    0,  224,    2, 0x08 /* Private */,
-      28,    0,  225,    2, 0x08 /* Private */,
-      29,    0,  226,    2, 0x08 /* Private */,
-      30,    0,  227,    2, 0x08 /* Private */,
-      31,    0,  228,    2, 0x08 /* Private */,
-      32,    0,  229,    2, 0x08 /* Private */,
-      33,    0,  230,    2, 0x08 /* Private */,
-      34,    0,  231,    2, 0x08 /* Private */,
-      35,    0,  232,    2, 0x08 /* Private */,
-      36,    0,  233,    2, 0x08 /* Private */,
-      37,    0,  234,    2, 0x08 /* Private */,
-      38,    0,  235,    2, 0x08 /* Private */,
+       1,    1,  159,    2, 0x08 /* Private */,
+       3,    0,  162,    2, 0x08 /* Private */,
+       4,    0,  163,    2, 0x08 /* Private */,
+       5,    0,  164,    2, 0x08 /* Private */,
+       6,    0,  165,    2, 0x08 /* Private */,
+       7,    0,  166,    2, 0x08 /* Private */,
+       8,    0,  167,    2, 0x08 /* Private */,
+       9,    0,  168,    2, 0x08 /* Private */,
+      10,    0,  169,    2, 0x08 /* Private */,
+      11,    0,  170,    2, 0x08 /* Private */,
+      12,    0,  171,    2, 0x08 /* Private */,
+      13,    0,  172,    2, 0x08 /* Private */,
+      14,    0,  173,    2, 0x08 /* Private */,
+      15,    0,  174,    2, 0x08 /* Private */,
+      16,    0,  175,    2, 0x08 /* Private */,
+      17,    0,  176,    2, 0x08 /* Private */,
+      18,    0,  177,    2, 0x08 /* Private */,
+      19,    0,  178,    2, 0x08 /* Private */,
+      20,    0,  179,    2, 0x08 /* Private */,
+      21,    0,  180,    2, 0x08 /* Private */,
+      22,    0,  181,    2, 0x08 /* Private */,
+      23,    0,  182,    2, 0x08 /* Private */,
+      24,    0,  183,    2, 0x08 /* Private */,
+      25,    0,  184,    2, 0x08 /* Private */,
+      26,    0,  185,    2, 0x08 /* Private */,
+      27,    0,  186,    2, 0x08 /* Private */,
+      28,    0,  187,    2, 0x08 /* Private */,
+      29,    0,  188,    2, 0x08 /* Private */,
+      30,    0,  189,    2, 0x08 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    2,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -188,47 +164,38 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<MainWindow *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->s1(); break;
-        case 1: _t->s3(); break;
-        case 2: _t->s4(); break;
-        case 3: _t->s5(); break;
-        case 4: _t->b1(); break;
-        case 5: _t->b2(); break;
-        case 6: _t->b3(); break;
-        case 7: _t->b4(); break;
-        case 8: _t->b5(); break;
-        case 9: _t->b6(); break;
-        case 10: _t->b7(); break;
-        case 11: _t->b8(); break;
-        case 12: _t->b9(); break;
-        case 13: _t->b10(); break;
-        case 14: _t->on_insertCard_clicked(); break;
-        case 15: _t->on_screen1_clicked(); break;
-        case 16: _t->on_screen2_clicked(); break;
-        case 17: _t->on_screen3_clicked(); break;
-        case 18: _t->on_screen4_clicked(); break;
-        case 19: _t->on_screen5_clicked(); break;
-        case 20: _t->on_screen6_clicked(); break;
-        case 21: _t->on_screen7_clicked(); break;
-        case 22: _t->on_screen8_clicked(); break;
-        case 23: _t->on_reset_clicked(); break;
-        case 24: _t->on_b1_clicked(); break;
-        case 25: _t->on_b2_clicked(); break;
-        case 26: _t->on_b3_clicked(); break;
-        case 27: _t->on_b4_clicked(); break;
-        case 28: _t->on_b5_clicked(); break;
-        case 29: _t->on_b6_clicked(); break;
-        case 30: _t->on_b7_clicked(); break;
-        case 31: _t->on_b8_clicked(); break;
-        case 32: _t->on_b9_clicked(); break;
-        case 33: _t->on_ok_clicked(); break;
-        case 34: _t->on_cancel_clicked(); break;
-        case 35: _t->on_insertCash_clicked(); break;
-        case 36: _t->on_settingsBtn_clicked(); break;
+        case 0: _t->changeSumCash((*reinterpret_cast< const int(*)>(_a[1]))); break;
+        case 1: _t->s1(); break;
+        case 2: _t->s3(); break;
+        case 3: _t->s4(); break;
+        case 4: _t->s5(); break;
+        case 5: _t->on_insertCard_clicked(); break;
+        case 6: _t->on_screen1_clicked(); break;
+        case 7: _t->on_screen2_clicked(); break;
+        case 8: _t->on_screen3_clicked(); break;
+        case 9: _t->on_screen4_clicked(); break;
+        case 10: _t->on_screen5_clicked(); break;
+        case 11: _t->on_screen6_clicked(); break;
+        case 12: _t->on_screen7_clicked(); break;
+        case 13: _t->on_screen8_clicked(); break;
+        case 14: _t->on_reset_clicked(); break;
+        case 15: _t->on_b1_clicked(); break;
+        case 16: _t->on_b2_clicked(); break;
+        case 17: _t->on_b3_clicked(); break;
+        case 18: _t->on_b4_clicked(); break;
+        case 19: _t->on_b5_clicked(); break;
+        case 20: _t->on_b6_clicked(); break;
+        case 21: _t->on_b7_clicked(); break;
+        case 22: _t->on_b8_clicked(); break;
+        case 23: _t->on_b9_clicked(); break;
+        case 24: _t->on_ok_clicked(); break;
+        case 25: _t->on_cancel_clicked(); break;
+        case 26: _t->on_insertCash_clicked(); break;
+        case 27: _t->on_settingsBtn_clicked(); break;
+        case 28: _t->on_b0_clicked(); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
@@ -260,13 +227,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 37)
+        if (_id < 29)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 37;
+        _id -= 29;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 37)
+        if (_id < 29)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 37;
+        _id -= 29;
     }
     return _id;
 }
